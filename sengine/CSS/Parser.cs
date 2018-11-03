a1885d65a2425fd998b46db5f8a01baf36efda52
Sential <sentialx@gmail.com>
2018-11-03 19:15:46 +0100

    Add Parser.cs back

---
+++ sengine/CSS/Parser.cs
@@ -0,0 +1,41 @@
+﻿namespace sengine.CSS {
+    public partial class Parser {
+        public static StyleSheet Parse(string code) {
+            var styleSheet = new StyleSheet();
+
+            string capturedText = "";
+            string capturedCode = "";
+
+            StyleRule styleRule = new StyleRule();
+            StyleDeclaration rule = new StyleDeclaration();
+
+            for (int i = 0; i < code.Length; i++) {
+                capturedCode += code[i];
+
+                if (code[i] == '{') {
+                    styleRule = new StyleRule() {
+                        SelectorText = capturedText.Trim()
+                    };
+                    capturedText = "";
+                } else if (code[i] == ':') {
+                    rule = new StyleDeclaration() {
+                        Property = capturedText.Trim()
+                    };
+                    capturedText = "";
+                } else if (code[i] == ';') {
+                    rule.Value = capturedText.Trim();
+                    styleRule.Rules.Add(rule);
+                    capturedText = "";
+                } else if (code[i] == '}') {
+                    styleRule.CssText = capturedCode.Trim();
+                    styleSheet.Rules.Add(styleRule);
+                    capturedCode = "";
+                } else {
+                    capturedText += code[i];
+                }
+            }
+
+            return styleSheet;
+        }
+    }
+}
