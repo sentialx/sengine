ac9994a71fae76584af1fc2f268dc64c944160f4
unknown <xnerhu@gmail.com>
2018-11-03 23:52:52 +0100

    Improve printer

--- sengine/HTML/Printer.cs
+++ sengine/HTML/Printer.cs
@@ -3,9 +3,16 @@ using System.Collections.Generic;
 
 namespace sengine.HTML {
     public class Utils {
+        private static ConsoleColor
+            TagColor = ConsoleColor.Blue,
+            TextColor = ConsoleColor.White,
+            CommentColor = ConsoleColor.DarkGray;
+
         public static void Print(List<DOMElement> tree, bool printClosing = true) {
             int lastLevel = 0;
+
             PrintChildren(tree, printClosing, ref lastLevel);
+            Console.ResetColor();
         }
 
         private static void PrintChildren(List<DOMElement> tree, bool printClosing, ref int lastLevel, int level = 0) {
@@ -15,20 +22,27 @@ namespace sengine.HTML {
                 string gap = new string(' ', 2 * level);
 
                 if (node.NodeType == NodeType.Text) {
-                    Console.WriteLine(gap + node.NodeValue);
+                    PrintColored(gap + node.NodeValue, TextColor);
+                } else if (node.NodeType == NodeType.Comment) {
+                    PrintColored(gap + string.Format("<!--{0}-->", node.NodeValue), CommentColor);
                 } else if (node.NodeType == NodeType.Element) {
-                    Console.WriteLine(string.Format("{0}<{1}>", gap, node.TagName));
+                    PrintColored(string.Format("{0}<{1}>", gap, node.TagName), TagColor);
 
                     if (node.Children.Count > 0) {
                         PrintChildren(node.Children, printClosing, ref lastLevel, level + 1);
                     }
-                }
 
-                if (printClosing && (level < lastLevel || level == 0) && node.TagName != null && node.TagName.Trim().Length > 0) {
-                    Console.WriteLine(string.Format("{0}</{1}>", gap, node.TagName));
+                    if (printClosing && (level < lastLevel || level == 0 || node.ParentNode.Children.Count - 1 == 0)) {
+                        PrintColored(string.Format("{0}</{1}>", gap, node.TagName), TagColor);
+                    }
                 }
-
             }
+        }
+
+        private static void PrintColored(string text, ConsoleColor color, bool inline = false) {
+            Console.ForegroundColor = color;
+            if (!inline) Console.WriteLine(text);
+            else Console.Write(text);
         }
 
         public static void PrintTokens(List<string> tokens) {
